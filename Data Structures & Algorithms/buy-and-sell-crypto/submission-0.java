36a77a2fa30a0732fdb5df6a294e385191be2c9a
AvnishSharma00 <163812835+AvnishSharma00@users.noreply.github.com>
2026-05-25 19:07:38 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+          int max=0;
+          int min=Integer.MAX_VALUE;
+          for(int x:prices){
+              if(x<min){
+                min=x;
+              }
+              max=Math.max(max,x-min);
+          }
+          return max;
+    }
+}
