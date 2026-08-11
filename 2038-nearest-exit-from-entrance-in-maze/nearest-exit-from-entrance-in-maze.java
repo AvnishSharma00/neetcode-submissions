a2eee0768801b62c86df830b1b06ee3091ac0af4
AvnishSharma00 <163812835+AvnishSharma00@users.noreply.github.com>
2026-08-11 22:16:53 +0530

    Time: 7 ms (61.26%) | Memory: 48.5 MB (12.65%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+           int m=maze.length;
+           int n=maze[0].length;
+           Queue<int[]> queue=new LinkedList<>();
+           queue.offer(new int[]{entrance[0],entrance[1]});
+           maze[entrance[0]][entrance[1]]='+';
+           int[][] directions={
+                    {1,0},
+                    {0,1},
+                    {-1,0},
+                    {0,-1}
+           };
+           int steps=0;
+           while(!queue.isEmpty()){
+                 int size=queue.size();
+                 steps++;
+                 for(int i=0;i<size;i++){
+                     int current[]=queue.poll();
+                     int row=current[0];
+                     int col=current[1];
+                     for(int dir[]:directions){
+                         int newRow=row+dir[0];
+                         int newCol=col+dir[1];
+                         if(newRow<0 || newRow>=m|| newCol<0 || newCol>=n){
+                            continue;
+                         }
+                         if(maze[newRow][newCol]=='+'){
+                            continue;
+                         }
+                         if(newRow==0 || newRow==m-1 || newCol==0|| newCol==n-1){
+                            return steps;
+                         }
+                         maze[newRow][newCol]='+';
+                         queue.offer(new int[]{newRow,newCol});
+                     }
+                 }
+           }
+           return -1;
+    }
+}
